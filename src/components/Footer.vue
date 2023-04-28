4ee6267b23a6c9d0aedb758c7b9c6f00c2386ed5
Mario Garcia <msgimanila@yahoo.com>
2023-04-28 08:01:03 +0800

    Update Footer.vue
    1

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <div
     class="w-full shadow-lg bg-gray-100 dark:bg-gray-800 px-8 py-6 lg:flex justify-between items-center"
   >
-    <p class="mb-2 lg:mb-0">© Copyright 2020</p>
+    <p class="mb-2 lg:mb-0">© Copyright 2023</p>
 
     <div class="flex">
       <!--<a href="#" class="mr-6 hover:text-gray-400">{{ $t("footer.terms") }}</a>
